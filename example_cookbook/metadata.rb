70a90f855cf6e704e2fe5c8347389ae482f5f83b
Stephen Rufle <stephen.p.rufle@cox.net>
2015-11-02 19:52:04 -0700

    Initial Commit

---
+++ example_cookbook/metadata.rb
@@ -0,0 +1,7 @@
+name 'example_cookbook'
+maintainer 'Stephen Rufle'
+maintainer_email 'srufle@gmail.com'
+license 'GPL3.0'
+description 'Installs/Configures example_cookbook'
+long_description 'Installs/Configures example_cookbook'
+version '0.1.0'
